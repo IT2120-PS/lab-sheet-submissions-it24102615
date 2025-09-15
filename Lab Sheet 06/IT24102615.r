6c1ec49882680c56f0875c36baf69daee86c289f
it24102615 <it24102615@my.sliit.lk>
2025-09-15 10:50:48 +0530

    IT24102615_Lab06

---
+++ Lab Sheet 06/IT24102615.r
@@ -0,0 +1,24 @@
+getwd()
+
+setwd("C:\\Users\\ASUS\\Desktop\\Lab_06_PS\\IT24102615")
+
+##Exercises
+#1
+
+#i)
+#Binomial Distribution
+#X ~ Bin(n = 50, p = 0.85)
+
+#ii)
+pbinom(46, size = 50, prob = 0.85, lower.tail = FALSE)
+
+#2
+
+#i)
+#X ~ Poisson(λ=12 per hour)
+
+#ii)
+#Poisson Distribution
+
+#iii)
+dpois(15, lambda = 12)
